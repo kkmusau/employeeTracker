5d6138a755371f450cde0ae0e694f9fd1494d89a
kkmusau <kkmusau@gmail.com>
2022-06-28 23:08:58 -0500

    added employee database to schema.sql

--- Main/index.js
+++ Main/index.js
@@ -1,3 +1,14 @@
 const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
+
+
+// Connection to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employees'
+}, 
+console.log(`Connected to the employee database.`)
+);
